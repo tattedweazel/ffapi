c33fff2311b830d523d6fd03e482a437f1f9b02d
Dave Pratt <dave@paracore.com>
2014-07-01 21:10:37 -0700

    Updating DB name

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'cities',
+			'database'  => 'ffapi',
 			'username'  => 'root',
 			'password'  => 'root',
 			'charset'   => 'utf8',
